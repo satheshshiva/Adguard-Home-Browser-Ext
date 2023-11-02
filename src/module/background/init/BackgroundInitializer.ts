2b357de60b0e02cede8d95ed97412270fe04b3ac
Sathesh Sivashanmugam <satheshshiva@gmail.com>
2023-11-01 17:35:02 -0700

    background check changed to alarms instead of window.setInterval

--- src/module/background/init/BackgroundInitializer.ts
+++ src/module/background/init/BackgroundInitializer.ts
@@ -1,42 +1,18 @@
 import {
   BadgeService,
-  ExtensionBadgeTextEnum
 } from '../../../service/BadgeService'
 import ContextMenuInitializer from './ContextMenuInitializer'
 import ChromeRuntimeInitializer from './ChromeRuntimeInitializer'
 import { Initializer } from '../../general/Initializer'
-import AdGuardApiService from '../../../service/AdGuardApiService'
-import AdGuardApiStatusEnum from '../../../api/enum/AdGuardApiStatusEnum'
 import HotKeyInitializer from './HotKeyInitializer'
+import CheckStatusInitializer from "./CheckStatusInitializer";
 
 export default class BackgroundInitializer implements Initializer {
-  private readonly INTERVAL_TIMEOUT = 15000
-
   public init(): void {
     BadgeService.setBadgeText('')
-
     new ContextMenuInitializer().init()
     new ChromeRuntimeInitializer().init()
     new HotKeyInitializer().init()
-
-    this.checkStatus().then()
-    window.setInterval(() => this.checkStatus(), this.INTERVAL_TIMEOUT)
-  }
-
-  /**
-   * Checking the current status of the AdGuard instance(s)
-   */
-  private async checkStatus(): Promise<void> {
-    AdGuardApiService.getAdGuardStatusCombined().then(value => {
-      BadgeService.getBadgeText().then(result => {
-        if (!BadgeService.compareBadgeTextToApiStatusEnum(result, value)) {
-          if (value === AdGuardApiStatusEnum.disabled) {
-            BadgeService.setBadgeText(ExtensionBadgeTextEnum.disabled)
-          } else if (value === AdGuardApiStatusEnum.enabled) {
-            BadgeService.setBadgeText(ExtensionBadgeTextEnum.enabled)
-          }
-        }
-      })
-    })
+    new CheckStatusInitializer().init()
   }
 }
---
+++ src/module/background/init/CheckStatusInitializer.ts
@@ -0,0 +1,40 @@
+import { Initializer } from "../../general/Initializer";
+import AdGuardApiService from '../../../service/AdGuardApiService'
+import AdGuardApiStatusEnum from '../../../api/enum/AdGuardApiStatusEnum'
+import { BadgeService, ExtensionBadgeTextEnum } from "../../../service/BadgeService";
+export default class CheckStatusInitializer implements Initializer {
+
+  private readonly INTERVAL_TIMEOUT = 0.3;
+
+  public init(): void {
+    console.log(("CheckStatusInitializer::init() called"));
+    this.checkStatus().then();
+    chrome.alarms.create("BadgeTextStatusChecker", {  periodInMinutes: this.INTERVAL_TIMEOUT });
+
+    chrome.alarms.onAlarm.addListener(() => {
+      this.checkStatus().then();
+    });
+
+    chrome.runtime.onSuspend.addListener(() => {
+      console.warn("Unloading CheckStatusInitializer::");
+    });
+  }
+
+  /**
+   * Checking the current status of the AdGuard instance(s)
+   */
+  private async checkStatus(): Promise<void> {
+    console.log(("CheckStatusInitializer::checkStatus() called"))
+    AdGuardApiService.getAdGuardStatusCombined().then(value => {
+      BadgeService.getBadgeText().then(result => {
+        if (!BadgeService.compareBadgeTextToApiStatusEnum(result, value)) {
+          if (value === AdGuardApiStatusEnum.disabled) {
+            BadgeService.setBadgeText(ExtensionBadgeTextEnum.disabled)
+          } else if (value === AdGuardApiStatusEnum.enabled) {
+            BadgeService.setBadgeText(ExtensionBadgeTextEnum.enabled)
+          }
+        }
+      })
+    })
+  }
+  }
